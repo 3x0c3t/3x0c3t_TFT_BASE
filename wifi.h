fed124c01ecb1a11310697ec56e3fb8720dfb10f
3x0c3t <jcalonne.prv@gmail.com>
2026-08-09 18:49:18 +0200

    Restore v1.0 configuration consistency

--- wifi.h
+++ wifi.h
@@ -3,11 +3,24 @@
 
 #include <Arduino.h>
 
+enum WifiMode
+{
+    WIFI_MODE_DISCONNECTED,
+    WIFI_MODE_CLIENT,
+    WIFI_MODE_AP
+};
+
+extern WifiMode wifiMode;
 extern bool wifiConnected;
 
 void wifiInit();
 void wifiUpdate();
 
 bool wifiIsConnected();
+bool wifiIsAP();
+
+String wifiGetIP();
+String wifiGetSSID();
 
 #endif
+
